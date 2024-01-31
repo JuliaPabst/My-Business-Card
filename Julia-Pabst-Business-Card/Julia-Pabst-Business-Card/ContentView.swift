caf314db4b823cec911a2802c2dfe3809ea207b5
Jules <ju.pabst@gmail.com>
2024-01-31 13:06:37 +0100

    added image

--- Julia-Pabst-Business-Card/Julia-Pabst-Business-Card/ContentView.swift
+++ Julia-Pabst-Business-Card/Julia-Pabst-Business-Card/ContentView.swift
@@ -11,7 +11,12 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(red: 0.09, green: 0.63, blue: 0.52).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Julia Pabst").font(Font.custom("Avenir", size: 40)).bold().foregroundColor(.white)
+            VStack{
+                Image("Jules1").resizable().aspectRatio(contentMode: .fit
+                ).frame(width: 150, height: 150).clipShape(Circle())
+                Text("Julia Pabst").font(Font.custom("Avenir", size: 50)).bold().foregroundColor(.white)
+                Text("iOS Developer").font(Font.custom("Avenir", size: 30)).foregroundColor(.white)
+            }
         }
     }
 }
