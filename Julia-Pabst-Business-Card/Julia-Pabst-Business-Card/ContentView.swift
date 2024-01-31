ba14c9f191aa025723ce920e90f1a5b88f4d3dfd
Jules <ju.pabst@gmail.com>
2024-01-31 18:23:45 +0100

    extracted view ContactInformation and added phone number and email address

--- Julia-Pabst-Business-Card/Julia-Pabst-Business-Card/ContentView.swift
+++ Julia-Pabst-Business-Card/Julia-Pabst-Business-Card/ContentView.swift
@@ -26,16 +26,8 @@ struct ContentView: View {
                     .font(Font.custom("Avenir", size: 30))
                     .foregroundColor(.white)
                 Divider()
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(Color.white)
-                    .frame(height: 50)
-                    .overlay(HStack {
-                        Image(systemName: "phone.fill").foregroundColor(Color(red: 0.09, green: 0.63, blue: 0.52))
-                        Text("+43 660 1256182")
-                            .font(Font.custom("Avenir", size: 20))
-                            .bold()
-                            .foregroundColor(.black)
-                    })
+                ContactInformation(text: "+43 660 1256182", imageName: "phone.fill")
+                ContactInformation(text: "ju.pabst@gmail.com", imageName: "envelope.fill")
             }
         }
     }
@@ -43,4 +35,21 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ContactInformation: View {
+    let text: String
+    let imageName: String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName).foregroundColor(Color(red: 0.09, green: 0.63, blue: 0.52))
+                Text(text)
+                    .font(Font.custom("Avenir", size: 20))
+                    .bold()
+                    .foregroundColor(.black)
+            }).padding(.all)
+    }
 }
