9c49e56899c29372eeb91b451bc9b347527d1fc4
Jules <ju.pabst@gmail.com>
2024-01-31 11:52:11 +0100

    created X-Code-Project

---
+++ Julia-Pabst-Business-Card/Julia-Pabst-Business-Card/Julia_Pabst_Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Julia_Pabst_Business_CardApp.swift
+//  Julia-Pabst-Business-Card
+//
+//  Created by Julia Pabst on 31.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Julia_Pabst_Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
